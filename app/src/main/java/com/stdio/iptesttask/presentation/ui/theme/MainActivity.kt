e30f2eceda9edcd58833a5278e6d5fdede6eddfb
Std1o <37378410+Std1o@users.noreply.github.com>
2025-03-01 14:29:08 +0300

    lazy row for tags

--- app/src/main/java/com/stdio/iptesttask/presentation/ui/theme/MainActivity.kt
+++ app/src/main/java/com/stdio/iptesttask/presentation/ui/theme/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,15 +11,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
-import com.stdio.iptesttask.presentation.viewmodel.ProductViewModel
-import dagger.hilt.android.AndroidEntryPoint
-import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
@@ -28,12 +19,21 @@ import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.stdio.iptesttask.extensions.iTems
+import com.stdio.iptesttask.presentation.viewmodel.ProductViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -82,11 +82,27 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                                 tint = Purple40
                             )
                             Icon(
-                                modifier = Modifier.padding(10.dp, 0.dp, 0.dp ,0.dp),
+                                modifier = Modifier.padding(start = 10.dp),
                                 imageVector = Icons.Default.Delete,
                                 contentDescription = "Delete Icon",
                                 tint = Color.Red
                             )
+                        }
+                    }
+                    Row {
+                        LazyRow {
+                            iTems(item.tags.split(","), key = { it }) { tag ->
+                                OutlinedButton(
+                                    onClick = {}, shape = RoundedCornerShape(16),
+                                    modifier = Modifier.padding(end = 10.dp)
+                                ) {
+                                    Text(
+                                        tag.replace("[", "")
+                                            .replace("\"", "")
+                                            .replace("]", "")
+                                    )
+                                }
+                            }
                         }
                     }
                 }
