8ba6a4677b6f2486f2e79fb4304626b9ed4a02f5
Std1o <37378410+Std1o@users.noreply.github.com>
2025-03-01 18:48:31 +0300

    circle plus and minus icons

---
+++ app/src/main/java/com/stdio/iptesttask/presentation/ui/components/CircleMinus.kt
@@ -0,0 +1,59 @@
+package com.stdio.iptesttask.presentation.ui.components
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+
+val CircleMinus: ImageVector
+    get() {
+        if (_CircleMinus != null) {
+            return _CircleMinus!!
+        }
+        _CircleMinus = ImageVector.Builder(
+            name = "CircleMinus",
+            defaultWidth = 24.dp,
+            defaultHeight = 24.dp,
+            viewportWidth = 24f,
+            viewportHeight = 24f
+        ).apply {
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(22f, 12f)
+                arcTo(10f, 10f, 0f, isMoreThanHalf = false, isPositiveArc = true, 12f, 22f)
+                arcTo(10f, 10f, 0f, isMoreThanHalf = false, isPositiveArc = true, 2f, 12f)
+                arcTo(10f, 10f, 0f, isMoreThanHalf = false, isPositiveArc = true, 22f, 12f)
+                close()
+            }
+            path(
+                fill = null,
+                fillAlpha = 1.0f,
+                stroke = SolidColor(Color(0xFF000000)),
+                strokeAlpha = 1.0f,
+                strokeLineWidth = 2f,
+                strokeLineCap = StrokeCap.Round,
+                strokeLineJoin = StrokeJoin.Round,
+                strokeLineMiter = 1.0f,
+                pathFillType = PathFillType.NonZero
+            ) {
+                moveTo(8f, 12f)
+                horizontalLineToRelative(8f)
+            }
+        }.build()
+        return _CircleMinus!!
+    }
+
+private var _CircleMinus: ImageVector? = null
--- app/src/main/java/com/stdio/iptesttask/presentation/ui/screens/main/AlertChangeAmountDialog.kt
+++ app/src/main/java/com/stdio/iptesttask/presentation/ui/screens/main/AlertChangeAmountDialog.kt
@@ -1,5 +1,6 @@
 package com.stdio.iptesttask.presentation.ui.screens.main
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.AlertDialog
@@ -10,6 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import com.stdio.iptesttask.R
+import com.stdio.iptesttask.presentation.ui.components.CircleMinus
+import com.stdio.iptesttask.presentation.ui.components.CirclePlus
+import com.stdio.iptesttask.presentation.ui.theme.Purple40
 
 @Composable
 fun AlertChangeAmountDialog(onDismissRequest: () -> Unit) {
@@ -24,21 +28,32 @@ fun AlertChangeAmountDialog(onDismissRequest: () -> Unit) {
             Text(text = stringResource(R.string.amount_dialog))
         },
         text = {
-
+            Row {
+                Icon(
+                    imageVector = CircleMinus,
+                    contentDescription = "Change Amount Icon",
+                    tint = Purple40
+                )
+                Icon(
+                    imageVector = CirclePlus,
+                    contentDescription = "Change Amount Icon",
+                    tint = Purple40
+                )
+            }
         },
         onDismissRequest = { onDismissRequest() },
         confirmButton = {
             TextButton(
                 onClick = { onDismissRequest() }
             ) {
-                Text(stringResource(id = R.string.apply))
+                Text(stringResource(id = R.string.apply), color = Purple40)
             }
         },
         dismissButton = {
             TextButton(
                 onClick = { onDismissRequest() }
             ) {
-                Text(stringResource(id = R.string.cancel))
+                Text(stringResource(id = R.string.cancel), color = Purple40)
             }
         },
         containerColor = Color.White
