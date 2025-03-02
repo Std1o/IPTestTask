ed6520286df75e4a5f037bb44fc2d80aab2174d0
Std1o <37378410+Std1o@users.noreply.github.com>
2025-03-02 11:29:53 +0300

    plugins to toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    id("com.google.devtools.ksp")
-    id("com.google.dagger.hilt.android")
-    id("kotlin-parcelize")
+    alias(libs.plugins.devtools.ksp)
+    alias(libs.plugins.dagger.hilt)
+    alias(libs.plugins.kotlin.parcelize)
 }
 
 android {
